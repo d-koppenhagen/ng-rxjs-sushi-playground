adb819b4dbf89c60ad0ba701b74dcb30406549f3
Danny Koppenhagen <mail@d-koppenhagen.de>
2019-12-13 20:05:46 +0100

    some styling stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { PlateComponent } from './plate/plate.component';
 import { SushiComponent } from './sushi/sushi.component';
